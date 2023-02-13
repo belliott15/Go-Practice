d7ac7667a52fc765f36113828d8a7e541462dd80
Beau Elliott <belliott1516@gmail.com>
2023-02-13 14:06:47 -0800

    added conditionals to monitor user input

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
-import "fmt" 
+//when importing multiple packages you lock them in parenthesis and put them on their own line
+import (
+	"fmt"
+	"strings"
+	) 
 
 func main(){
+	//different syntax for assigning variables
 	conferenceName := "Go Conference"
 	const conferenceTickets = 50
+	//uint will not take negative integers
 	var remainingTickets uint = 50
 	var bookings = []string{}
 
@@ -11,31 +17,61 @@ func main(){
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here!")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	// ask user for name
-	fmt.Println("Please enter your first name: ")
-	fmt.Scan(&firstName)
+	//for loops have a default true conditional that can be altered to fit your own conditionals
+	for remainingTickets > 0 && len(bookings) < 50 {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		// ask user for information
+		fmt.Println("Please enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Please enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Please enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Please enter your email address: ")
-	fmt.Scan(&email)
-	
-	fmt.Println("Please enter the number of tickets desired ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Please enter your email address: ")
+		fmt.Scan(&email)
+		
+		fmt.Println("Please enter the number of tickets desired ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName + " " + lastName)
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2 
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-	fmt.Printf("The whole Slice: %v\n", bookings)
-	fmt.Printf("The first Value: %v\n", bookings[0])
-	fmt.Printf("The slice type: %T\n", bookings)
-	fmt.Printf("The slice length: %v\n", len(bookings))
+		if isValidEmail && isValidName && isValidTicketNumber {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName + " " + lastName)
 
-	 fmt.Printf("Thank you %v %v for booking %v tickets.  You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email);
-	 fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets.  You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email);
+			fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			fmt.Printf("The first names of all our bookings: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Sorry but this event has been sold out.")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Printf("The first or last name you entered is not greater than two characters. \n")
+			}
+
+			if !isValidEmail {
+				fmt.Printf("The email you entered does not contain @. \n")
+			}
+
+			if !isValidTicketNumber {
+				fmt.Printf("Number of tickets requested is invalid. \n")
+			}
+		}
+	}	
 }
